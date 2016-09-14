cf00222a5c1b3ab8c09d4f9aebe0090c72d7024d
HolaJam <jamcplusplus@gmail.com>
2016-09-14 11:36:02 +0800

    修正了代码，感谢美男子的指正

--- run.py
+++ run.py
@@ -1,106 +1,73 @@
 #-*-coding:utf-8-*-
 #!/usr/bin/env python
-# Created by Jam on 2016/8/30.
+# Created by Jam on 2016/9/13.
+
 import requests
-from bs4 import BeautifulSoup
-import random
-import os
-import string
 import threading
-import muti
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import os
+import random
+import ua
+from bs4 import BeautifulSoup
 
 
-_user_agent = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"]
+def max_num():
+    r = requests.get(url='http://www.luoo.net/music/')
+    soup = BeautifulSoup(r.content, 'html.parser', from_encoding='utf8')
+    node = soup.find('div', {'class': 'item'})
+    _max = node.find('a')['href'].split('/')[-1]
+    return _max
+
+max_num = max_num()
+work_queue = range(1, int(max_num) + 1)
 
 
-class fun():
+def page_source(page_id):
+    _ua = random.choice(ua.user_agent)
+    headers = {
+        'User-Agent': _ua
+    }
+    r = requests.get('http://www.luoo.net/music/' +
+                     str(page_id), headers=headers)
+    soup = BeautifulSoup(
+        r.content,
+        'html.parser',
+        from_encoding='utf8')
+    return soup
 
-    def __init__(self, page_id):
-        self.page_id = str(page_id)
 
-    # 获得网页源码
-    def page_source(self):
-        ua = random.choice(_user_agent)
-        headers = {
-            'User-Agent': ua
-        }
-        r = requests.get('http://www.luoo.net/music/' +
-                         str(self.page_id), headers=headers)
-        return r.content
+class img(threading.Thread):
 
-    # 网页源码转为bs对象
-    def turn_to_soup(self):
-        soup = BeautifulSoup(
-            self.page_source(),
-            'html.parser',
-            from_encoding='utf8')
-        return soup
+    def __init__(self):
+        threading.Thread.__init__(self)
 
-    # 创建期刊编号文件夹
-    def mkdir(self, alt):
-        i = str(self.page_id)
-        s = str(alt)
-        name = u'vol.%s《%s》' % (i, s)
-        if os.path.exists(name):
-            os.chdir(name)
-        else:
-            os.makedirs(name)
-            os.chdir(name)
+    def run(self):
+        def download(page_id):
+            try:
+                img_url = page_source(
+                    page_id).find(
+                    'img', {
+                        'class': 'vol-cover'})
+                r = requests.get(img_url['src'])
+                with open(img_url['alt'] + '.jpg', 'wb') as img:
+                    img.write(r.content)
+            except Exception as e:
+                print str(e)
+        for i in work_queue:
+            download(i)
 
-    # 下载期刊图片
-    def page_img(self):
-        try:
-            img_url = self.turn_to_soup().find('img', {'class': 'vol-cover'})
-            r = requests.get(img_url['src'])
-            self.mkdir(img_url['alt'])
-            with open(img_url['alt'] + '.jpg', 'wb') as img:
-                img.write(r.content)
-        except Exception as e:
-            print str(e)
-            os.makedirs(self.page_id)
-            return
 
-    # 下载歌曲
-    def page_song(self):
-        try:
-            songs = self.turn_to_soup().find_all(
+class song(img):
+
+    def __init__(self):
+        super(img, self).__init__()
+
+    def run(self):
+        def download(page_id):
+            _ua = random.choice(ua.user_agent)
+            headers = {
+                'User-Agent': _ua
+            }
+            songs = page_source(page_id).find_all(
                 'div', {'class': 'player-wrapper'})
             song_list = []
             for song in songs:
@@ -116,41 +83,19 @@ class fun():
                 if os.path.exists(song + '.mp3'):
                     pass
                 else:
+                    url = 'http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio' + \
+                        str(page_id) + '/' + str(song_num) + '.mp3'
+                    r = requests.get(url, headers=headers)
                     try:
-
-                        url = 'http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio' + \
-                            self.page_id + '/' + str(song_num) + '.mp3'
-                        '''
-                        r = requests.get(url)
                         with open(song + '.mp3', 'wb') as song:
                             song.write(r.content)
-                        '''
-                        song_num += 1
-                        muti.download_file(url=url, file_name=song + '.mp3')
+
                     except Exception as e:
+                        print str(e)
                         with open(song + '.mp3', 'wb') as song:
                             song.write(r.content)
+        for i in work_queue:
+            download(i)
 
-        except Exception as e:
-            print str(e)
-        os.chdir('../')
-
-    # 调用
-    def run(self):
-        threads = []
-        task1 = threading.Thread(target=self.page_img())
-        threads.append(task1)
-        task2 = threading.Thread(target=self.page_song())
-        threads.append(task2)
-
-        for t in threads:
-            t.setDaemon(True)
-            t.start()
-fail = []
-page_id = 1
-while True:
-    luowang = fun(page_id=page_id)
-    print u'正在下载第 %s 期...' % str(page_id)
-    luowang.run()
-    print u'第 %s 期下载完成。' % str(page_id)
-    page_id += 1
+thread0 = song()
+thread0.start()
