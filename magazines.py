6ec21b300f1f0863bf4c5c4ee47fd9bf5c83f603
HolaJam <jamcplusplus@gmail.com>
2017-01-17 18:23:22 +0800

    尝试使用生产者-消费者重建

--- magazines.py
+++ magazines.py
@@ -1,165 +1,58 @@
 #-*-coding:utf-8-*-
 #!/usr/bin/env python
 # Created by Jam on 2016/9/13.
-
-import requests
-import time
-import threading
 import os
+import time
 import random
-import ua
+import queue
+import requests
+import threading
 from bs4 import BeautifulSoup
-import gevent
 
-# 获得落网最新期刊编号
+page_queue = queue.Queue()
 
+class spider(object):
+    def __init__(self, url):
+        self.headers = {
+            'Host':
+                'www.luoo.net',
+            'Referer':
+                'http://www.luoo.net/',
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+        }
+        self.url = url
+    def page_source(self):
+        r = requests.get(url=self.url, headers=self.headers)
+        res = (r.text.encode(r.encoding).decode('utf8'))
+        soup = BeautifulSoup(res, 'html.parser')
+        return soup
 
-def max_num():
-    r = requests.get(url='http://www.luoo.net/music/')
-    soup = BeautifulSoup(r.content, 'html.parser', from_encoding='utf8')
-    node = soup.find('div', {'class': 'item'})
-    _max = node.find('a')['href'].split('/')[-1]
-    return _max
-
-max_num = max_num()
-
-# 生成一个任务队列
-work = range(1, int(max_num) + 1)
-
-def work_queue():
-    for i in work:
-        yield i
-
-# 创建相应的文件目录
-
-
-def mkdir(name):
-    if os.path.exists(name):
-        pass
-    else:
-        os.makedirs(name)
-    os.chdir(name)
-
-# 获得某期刊的页面源码
-
-
-def page_source(page_id):
-    _ua = random.choice(ua.user_agent)
-    headers = {
-        'User-Agent': _ua
-    }
-    r = requests.get('http://www.luoo.net/music/' +
-                     str(page_id), headers=headers)
-    soup = BeautifulSoup(
-        r.content,
-        'html.parser',
-        from_encoding='utf8')
-    return soup
-
-# 线程：下载图片
-
-
-class img(threading.Thread):
-
+class page_producer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        threading.Thread.name = 'page_producer'
 
+    def create_queue(self):
+        spider4main_html = spider(url='http://www.luoo.net/tag/?p=1')
+        all_num = spider4main_html.page_source().find('div', {'class': 'item'}).find('a', {'class': 'cover-wrapper'})['href'].split('/')[-1]
+        print(all_num)
+        return int(all_num.strip())
     def run(self):
-        def download(page_id):
-            try:
-                img_url = page_source(
-                    page_id).find(
-                    'img', {
-                        'class': 'vol-cover'})
-                name = 'vol%s ' % (page_id) + img_url['alt']
-                mkdir(name)
-                r = requests.get(img_url['src'])
-                with open(img_url['alt'] + '.jpg', 'wb') as img:
-                    img.write(r.content)
-            except Exception as e:
-                print str(e)
+        for i in range(self.create_queue(), 1, -1):
+            print(i)
+            page_queue.put(i)
 
-        for i in work_queue():
-            download(i)
-            os.chdir('../')
-
-    def stop(self):
-        self.stopped = True
-
-    def is_stoped(self):
-        return self.stopped
-
-# 线程：下载歌曲
-
-
-class song(img):
-
+class page_consumer(threading.Thread):
     def __init__(self):
-        super(img, self).__init__()
+        threading.Thread.__init__(self)
+        threading.Thread.name = 'page_consumer'
 
     def run(self):
-        def download(page_id):
-            soup = page_source(page_id)
-            title = soup.find('title').text
-            title = 'vol%s ' % (page_id) + title.split('-')[0]
-            mkdir(title)
-            songs = soup.find_all(
-                'div', {'class': 'player-wrapper'})
-            song_list = []
-            for song in songs:
-                song_info = song.text.strip().replace('\n', '_')
-                song_info = song_info.replace(':', '-')
-                song_list.append(song_info)
-            song_num = 1
-
-            # 格式化文件名
-            name_reg = ['<', '>', '/', '\\', '|', '\"', '*', '?', '&', '$']
-            for song in song_list:
-                for i in name_reg:
-                    if i in song:
-                        song = song.replace(i, '')
-                if os.path.exists(song + '.mp3'):
-                    pass
-                else:
-                    url = 'http://luoo-mp3.kssws.ks-cdn.com/low/luoo/radio' + \
-                        str(page_id) + '/' + str(song_num) + '.mp3'
-
-                    local_name = song + '.mp3'
-                    r = requests.get(url, stream=True)
-
-                    try:
-                        # 断点重传
-                        with open(local_name, 'wb') as f:
-                            for chunk in r.iter_content(chunk_size=1024):
-                                if chunk:
-                                    f.write(chunk)
-                                    f.flush()
-
-                        time.sleep(random.randrange(1, 2))
-                    except Exception as e:
-                        print str(e)
-                        # 如果出现异常,那就直接下载吧～
-                        with open(local_name, 'wb') as song:
-                            song.write(r.content)
-                    song_num += 1
-                    time.sleep(random.randint(1, 10))
-        for i in work_queue():
-            download(i)
-            os.chdir('../')
-
-    def stop(self):
-        self.stopped = True
-
-    def is_stoped(self):
-        return self.stopped
-
-
-thread1 = img()
-thread1.start()
-thread1.join()
-thread1.stop()
-
-thread0 = song()
-thread0.start()
-thread0.join()
-thread0.stop()
+        while True:
+            page_id = page_queue.get()
+            # print(page_id)
+test_p = page_producer()
+test_p.start()
+test_c = page_consumer()
+test_c.start()
