b3d9ea32f750553c28e61259bc5b5b5c9ef5271d
jamcplusplus <jamcplusplus@gmail.com>
2017-02-05 23:56:28 +0800

    最简单的播放功能，仍然无法循环播放

--- player.py
+++ player.py
@@ -7,14 +7,18 @@ def play_music(folder):
     # 判断是否存在路径
     if os.path.exists(folder) is not True:
         return
+
+    def player(music_path):
+        print(music_path)
+        play = pyglet.media.load(music)
+        play.play()
+        pyglet.app.run()
+
+
     # 遍历路径中的音乐，循环播放
     for music in os.listdir(folder):
         music = '%s/%s' % (folder, music)
-        print(music)
-        play = pyglet.media.load(music)
-        play.play()
-        # todo 循环播放
-
+        player(music_path=music)
 
 def main(magazine_id):
     play_music(folder=str(magazine_id))
