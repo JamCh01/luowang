c2bdd81fae91acc16b95f3b29f8a2f54b4c93aa5
jamcplusplus <jamcplusplus@gmail.com>
2017-02-06 17:58:19 +0800

    完成播放器的最基本功能，支持列表播放，但播放一次就会退出，会在稍后的版本中修复

--- player.py
+++ player.py
@@ -1,28 +1,46 @@
 import os
 import pyglet
 # need AVbin10 to play music
+# you can download it from here -> http://avbin.github.io/AVbin/Download.html
 
 
-def play_music(folder):
-    # 判断是否存在路径
-    if os.path.exists(folder) is not True:
-        return
 
-    def player(music_path):
-        print(music_path)
-        play = pyglet.media.load(music)
-        play.play()
-        pyglet.app.run()
+class music_player(object):
+    '''
+    播放器类，播放音乐
+    '''
+    def __init__(self, music_list):
+        self.music_list = music_list
 
 
-    # 遍历路径中的音乐，循环播放
-    for music in os.listdir(folder):
-        music = '%s/%s' % (folder, music)
-        player(music_path=music)
+    @staticmethod
+    def __exiter(dt):
+        pyglet.app.exit()
+
+    def play(self):
+        # 遍历音乐列表中的音乐，循环播放
+        for music in self.music_list:
+            # 加载音乐
+            player = pyglet.media.load(music)
+            print('''正在播放‘{}’，总时长：{}s'''.format(music,str(player.duration)))
+            # 播放音乐
+            player.play()
+            # 设置延时任务，时间为正在播放的音乐时长，任务为退出pyglet
+            pyglet.clock.schedule_once(self.__exiter, player.duration)
+            # 启动pyglet
+            pyglet.app.run()
+
+
 
 def main(magazine_id):
-    play_music(folder=str(magazine_id))
+    magazine_id = str(magazine_id)
+    if os.path.exists(magazine_id) is not True:
+        return
+
+    player = music_player(music_list=['%s/%s' % (magazine_id, i) for i in  os.listdir(magazine_id)])
+    player.play()
 
 
 if __name__ == '__main__':
     main(magazine_id='886')
+    # todo 整理代码；完成列表循环；加入一个SQLite，作为路径存储和播放列表
