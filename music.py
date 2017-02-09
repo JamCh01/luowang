9e0ac97642cbe4cecfd023ad5e60afa38e13a8f3
jamcplusplus <jamcplusplus@gmail.com>
2017-02-09 17:27:44 +0800

    一次比较大的变动，增加了favorite_list，增加了数据检测类，修正了Windows平台下文件名规则

--- music.py
+++ music.py
@@ -2,16 +2,17 @@ import os
 import orm
 import sys
 import queue
+import platform
 import requests
 import threading
 from mutagen.mp3 import MP3
 from luoo import spider4id
 from luoo import mp3url
 from player import run_player
-from player import run_player_with_favorite
 
 download_queue = queue.Queue()
 download_lock = threading.Lock()
+file_name_ignore_list = ['/', '\\', ':', '"', '<', '>', '?', '|', '*']
 
 
 class download_producer(threading.Thread):
@@ -43,6 +44,29 @@ class download_producer(threading.Thread):
             base_url = mp3url(magazine_id=self.magazine_id)
             download_url = '%s%s.mp3' % (base_url, i)
             download_queue.put(download_url)
+        try:
+            magazine_name = soup.find('title').text.strip().replace('-落网', '')
+        except Exception as e:
+            magazine_name = 'UNKNOW'
+
+        try:
+            magazine_tag = soup.find(
+                'a', {'class': 'vol-tag-item'}).text.strip().replace('#', '')
+        except Exception as e:
+            magazine_tag = 'UNKNOW'
+
+        if check.check_magazine(magazine_id=magazine_id):
+            return
+        magazine_session = orm.DBSession()
+        magazine = orm.magazine(
+            magazine_id=self.magazine_id,
+            magazine_name=magazine_name,
+            magazine_tag=magazine_tag,
+            magazine_total=str(
+                len(music_player_node)))
+        magazine_session.add(magazine)
+        magazine_session.commit()
+        magazine_session.close()
 
 
 class download_consumer(threading.Thread):
@@ -55,8 +79,6 @@ class download_consumer(threading.Thread):
         threading.Thread.name = 'download_consumer'
 
     def run(self):
-        download_lock.release()
-        download_session = orm.DBSession()
         for i in range(download_queue.qsize()):
             song_url = download_queue.get(block=False)
             r = requests.get(url=song_url, stream=True)
@@ -70,24 +92,42 @@ class download_consumer(threading.Thread):
                 continue
             with open('%s/%s' % (save_path, song_name), 'wb') as song:
                 song.write(r.content)
+
             print('%s/%s' % (save_path, song_name))
             # 使用mutagen读取id3信息，更改歌曲名
             file = MP3('%s/%s' % (save_path, song_name))
             artist = str(
                 file['TPE1']).encode(
                 encoding='latin1').decode('gb18030')
-            new_name = str(
+            music_name = str(
                 file['TIT2']).encode(
                 encoding='latin1').decode('gb18030')
+
+            # 在Windows平台中，文件名不能包括某些字符
+            if 'Windows' in platform.system():
+                for i in file_name_ignore_list:
+                    music_name = music_name.replace(i, '')
+                    artist = artist.replace(i, '')
             try:
                 os.rename(
                     '%s/%s' %
                     (save_path, song_name), '%s/%s' %
                     (save_path, '%s-%s.mp3' %
-                     (artist, new_name)))
+                     (artist, music_name)))
             except Exception as e:
                 os.remove('%s/%s' % (save_path, song_name))
-            music_info = orm.music(music_name=new_name, magazine_id=magazine_id, music_artist=artist, music_save_path=magazine_id)
+            song_name = '%s-%s.mp3' % (artist, music_name)
+
+            if check.check_music(
+                    magazine_id=magazine_id,
+                    music_name=song_name):
+                return
+            download_session = orm.DBSession()
+            music_info = orm.music(
+                music_name=song_name,
+                magazine_id=magazine_id,
+                music_artist=artist,
+                music_save_path=magazine_id)
             download_session.add(music_info)
             download_session.commit()
             download_session.close()
@@ -98,12 +138,13 @@ def main(magazine_id):
     producer.start()
     producer.join()
     # 判断下载目录中是否存在与下载队列相同的文件数目（目前以这种方法判断了……）
-    # if download_queue.qsize() != len(os.listdir(path=magazine_id)):
-    #     consumer = download_consumer()
-    #     consumer.start()
-    #     consumer.join()
-    # run_player(folder=magazine_id)
-    run_player_with_favorite(folder=magazine_id)
+    if download_queue.qsize() != len(os.listdir(path=magazine_id)):
+        for i in range(int(download_queue.qsize())):
+            consumer = download_consumer()
+            consumer.start()
+            consumer.join()
+
+    run_player(folder=magazine_id)
 
 
 if __name__ == '__main__':
@@ -113,6 +154,5 @@ if __name__ == '__main__':
         pass
     else:
         os.makedirs(str(save_path))
-
+    check = orm.check()
     main(magazine_id=magazine_id)
-
--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import os
 import orm
+import time
 import shutil
 import pyglet
+import datetime
 
 # need AVbin10 to play music
 # you can download it from here -> http://avbin.github.io/AVbin/Download.html
@@ -12,8 +14,10 @@ class music_player(object):
     播放器类，播放音乐
     '''
 
-    def __init__(self, music_list):
+    def __init__(self, folder, music_list):
         self.music_list = music_list
+        self.folder = folder
+        self.check = orm.check()
 
     @staticmethod
     def __exiter(dt):
@@ -24,17 +28,43 @@ class music_player(object):
         播放器
         :return:
         '''
+
         # 遍历音乐列表中的音乐，循环播放
         for music in self.music_list:
-            # 加载音乐
-            player = pyglet.media.load(music)
-            print('''正在播放‘{}’，总时长：{}s'''.format(music, str(player.duration)))
+            try:
+                # 加载音乐
+                player = pyglet.media.load(music)
+            except Exception as e:
+                print('需要使用AVbin支持播放，下载地址->http://avbin.github.io/AVbin/Download.html')
+            play_time = player.duration
+            music_info_session = orm.DBSession()
+            music_info_session.query(orm.music).filter_by(
+                music_name=music.split('/')[1]).update({'play_time': int(play_time)})
+            music_info_session.flush()
+            music_info_session.commit()
+            music_info_session.close()
+
+            print('''正在播放‘{}’，总时长：{}s'''.format(music, str(play_time)))
             # 播放音乐
             player.play()
             # 设置延时任务，时间为正在播放的音乐时长，任务为退出pyglet（__exiter）
-            pyglet.clock.schedule_once(self.__exiter, player.duration)
+            pyglet.clock.schedule_once(self.__exiter, play_time)
             # 启动pyglet
             pyglet.app.run()
+
+        if self.folder == 'favorite':
+            pass
+        else:
+            if self.check.check_new(magazine_id=self.folder) == 1:
+                # self.favorite()
+                player_session = orm.DBSession()
+                player_session.query(orm.magazine.new).filter_by(
+                    magazine_id=self.folder).update({'new': 0})
+                player_session.flush()
+                player_session.commit()
+                player_session.close()
+        self.favorite_show()
+        self.favorite_add()
 
     def favorite(self):
         if os.path.exists('favorite'):
@@ -42,35 +72,56 @@ class music_player(object):
         else:
             os.makedirs('favorite')
 
+    def favorite_add(self):
+        self.favorite()
         print('本期期刊播放结束，请选择其中的歌曲编号加入喜欢列表（若无喜爱请输入pass）')
         for music in enumerate(self.music_list):
             music_name = music[1].split('/', 1)[1]
             print('编号:{}, 歌曲名:{}'.format(music[0], music_name))
         while True:
             tmp = input('->')
-            print(len(self.music_list))
-            print(self.music_list[0])
             if tmp == 'pass':
                 break
-            elif tmp in range(len(self.music_list)):
+            elif int(tmp) in range(0, len(self.music_list)):
+                tmp = int(tmp)
                 try:
                     favorite_session = orm.DBSession()
                     magazine_id = self.music_list[tmp].split('/', 1)[0]
                     music_name = self.music_list[tmp].split('/', 1)[1]
                     music_artist = music_name.split('-', 1)[0]
                     music_save_path = self.music_list[tmp]
-                    favorite_music = orm.music(
+                    if self.check.check_favorite(
+                            magazine_id=magazine_id, music_name=music_name):
+                        print('->这首歌已经在喜欢列表啦╭(′▽`)╭(′▽`)╯')
+                        continue
+                    favorite_music = orm.favorite(
                         music_name=music_name,
                         magazine_id=magazine_id,
                         music_artist=music_artist,
-                        music_save_path=music_save_path)
+                        music_save_path=music_save_path,
+                        add_time=datetime.datetime.now(),
+                        play_time=favorite_session.query(
+                            orm.music.play_time).filter_by(
+                            music_name=music_name)[0][0])
                     favorite_session.add(favorite_music)
                     favorite_session.commit()
                     favorite_session.close()
                 except Exception as e:
+                    print(e)
                     print('->好像没有诶')
+
             else:
                 print('->喵喵喵？')
+
+    def favorite_delete(self):
+        # todo 删除
+        self.favorite()
+
+    def favorite_show(self):
+        #todo 展示
+        show_session = orm.DBSession()
+        show_session.query(orm.favorite).all()
+
 
 
 def run_player(folder):
@@ -78,23 +129,10 @@ def run_player(folder):
     if os.path.exists(folder) is not True:
         return
     # 拼接歌曲存储地址
-    player = music_player(
-        music_list=[
-            '%s/%s' %
-            (folder, i) for i in os.listdir(folder)])
+    player = music_player(folder=folder,
+                          music_list=[
+                              '%s/%s' %
+                              (folder, i) for i in os.listdir(folder)])
     player.play()
-
-
-def run_player_with_favorite(folder):
-    folder = str(folder)
-    if os.path.exists(folder) is not True:
-        return
-    # 拼接歌曲存储地址
-    player = music_player(
-        music_list=[
-            '%s/%s' %
-            (folder, i) for i in os.listdir(folder)])
-    player.play()
-    player.favorite()
 
 # todo 完成列表循环；加入一个SQLite，作为路径存储和播放列表
